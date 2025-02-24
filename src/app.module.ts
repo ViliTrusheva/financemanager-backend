059819138f9908a746989af919d9af12556f35be
Vili <vili.trusheva@gmail.com>
2025-02-24 15:49:32 +0100

    package.json updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,25 +5,12 @@ import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { CategoriesModule } from './categories/categories.module';
 import { EntriesModule } from './entries/entries.module';
-import dbConfig from '../data.source';
+import dbConfig from '../ data.source';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
-        autoLoadEntities: true,
-        synchronize: true, // Setting synchronize: true shouldn't be used in production - otherwise you can lose production data.
-      }),
-      inject: [ConfigService],
-    }),
+    TypeOrmModule.forRoot(dbConfig.options), //.options, new compared to slides
     CategoriesModule,
     EntriesModule,
   ],
